8a10f6df5a380d1c35fd43a251ff134620820bbb
Qianlang Chen <qianlangchen@gmail.com>
2021-04-01 10:54:31 -0600

    5140 Asg 7 - All but EC (Q3)

--- cs5140/asg07/lib/FD.py
+++ cs5140/asg07/lib/FD.py
@@ -3,10 +3,24 @@ from scipy import linalg
 
 def freq_dir(A, l):
     n, d = A.shape
-    for a_i in A:
-        # TODO
-        pass
+    B = numpy.zeros((2 * l, d))
+    zero_index = 0
+    for i in range(n):
+        B[zero_index] = A[i]
+        zero_index += 1
+        if zero_index == 2 * l: # full
+            U, S, Vt = linalg.svd(B)
+            delta = S[-1]**2
+            S_prime = numpy.zeros((2 * l, d))
+            numpy.fill_diagonal(S_prime,
+                                [(sigma**2 - delta)**.5 for sigma in S])
+            B = S_prime @ Vt
+            for zero_index in range(2 * l):
+                if not B[zero_index].any(): break # the first empty row
+    return B
 
 if __name__ == '__main__':
     A = numpy.loadtxt('data/A.csv', delimiter=',')
-    freq_dir(A, 3)
+    B = freq_dir(A, 24)
+    print(linalg.norm(A)**2)
+    print(linalg.norm(A.T @ A - B.T @ B))
